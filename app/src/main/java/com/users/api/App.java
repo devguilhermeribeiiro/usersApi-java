54f9516a7684b5690cd533200b2f6cf02e320e95
devguilhermeribeiro <devguilhermeribeiro000@gmail.com>
2025-05-04 22:16:37 -0300

    Setting users controller routes

--- app/src/main/java/com/users/api/App.java
+++ app/src/main/java/com/users/api/App.java
@@ -1,14 +1,19 @@
 package com.users.api;
 
+import java.util.Map;
+
 import com.users.api.Config.AppConfig;
+import com.users.api.Controller.UserController;
 
 import io.javalin.Javalin;
 
 public class App {
     public static void main(String[] args) {
         Javalin app = AppConfig.setupConfig();
-        app.get("/", ctx -> ctx.result("Hello, world!"));
 
+        UserController.configRoutes(app);
+
+        app.get("/", ctx -> ctx.json(Map.of("message", "Hello, world!")));
         app.start(3000);
     }
 }
