eedfad53ecb3660bb5b9dcccbf566407d2a1a895
devguilhermeribeiro <devguilhermeribeiro000@gmail.com>
2025-05-05 22:24:14 -0300

    Setup connection method

---
+++ app/src/main/java/com/users/api/Config/Database.java
@@ -0,0 +1,31 @@
+package com.users.api.Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Database {
+
+    public static Connection connect() {
+        Connection conn = null;
+
+        Properties connectionProps = new Properties();
+        connectionProps.setProperty("url", "jdbc:postgres://localhost:5432/usersApi");
+        connectionProps.setProperty("user", "usersApi");
+        connectionProps.setProperty("password", "usersApi");
+
+        try {
+            conn = DriverManager.getConnection(
+                    connectionProps.getProperty("url"),
+                    connectionProps.getProperty("user"),
+                    connectionProps.getProperty("password"));
+
+            System.out.println("Get connected successfully");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+}
