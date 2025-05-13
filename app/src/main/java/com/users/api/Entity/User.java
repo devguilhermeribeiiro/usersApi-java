79d6fd294952f638ec0cbe7e8bfdeb1b935954fc
devguilhermeribeiro <devguilhermeribeiro000@gmail.com>
2025-05-13 02:48:05 -0300

    Setup Entity layer

--- app/src/main/java/com/users/api/Entity/User.java
+++ app/src/main/java/com/users/api/Entity/User.java
@@ -1,5 +1,31 @@
 package com.users.api.Entity;
 
+import java.util.UUID;
+
+import com.users.api.Dto.UserRequestDto;
+import org.jetbrains.annotations.NotNull;
+
 public class User {
+    private final UUID id;
+    private final String name;
+    private final String email;
+
+    public User(@NotNull String name, @NotNull String email) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 
 }
