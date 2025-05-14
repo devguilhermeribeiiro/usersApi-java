ce5cd3789b2f7696f913d133b266f753f828bd5d
devguilhermeribeiro <devguilhermeribeiro000@gmail.com>
2025-05-14 01:00:18 -0300

    Adding others restfull routes on controller

--- app/src/main/java/com/users/api/Controller/UserController.java
+++ app/src/main/java/com/users/api/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.users.api.Controller;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import com.users.api.Config.DependencyContainer;
 import com.users.api.Dto.UserRequestDto;
@@ -22,20 +23,48 @@ public class UserController {
     public void configRoutes(Javalin app) {
         app.get("/users", UserController::getUsers);
         app.post("/users", ctx -> {
-            UserController.create(ctx, ctx.bodyAsClass(UserRequestDto.class));
+            UserController.createUser(ctx, ctx.bodyAsClass(UserRequestDto.class));
         });
+        app.get("/users/{uuid}", ctx -> {
+            UserController.findUser(ctx, ctx.pathParam("uuid"));
+        });
+
+        app.post("/users/{uuid}", ctx -> {
+            UserController.updateUser(ctx, ctx.pathParam("uuid"), ctx.bodyAsClass(UserRequestDto.class));
+        });
+
+        app.post("/users/{uuid}/delete", ctx -> UserController.deleteUser(ctx, ctx.pathParam("uuid")));
     }
 
     private static void getUsers(Context ctx) throws SQLException {
-        List<UserResponseDto> responseDto = userService.getActives();
+        List<UserResponseDto> responseDto = userService.getUsers();
 
-        ctx.status(200).json(responseDto);
+        if (!responseDto.isEmpty()) ctx.status(200).json(responseDto);
+
+        ctx.status(204);
     }
 
-    private static void create(Context ctx, UserRequestDto dto) throws SQLException {
+    private static void createUser(Context ctx, UserRequestDto dto) throws SQLException {
         UserResponseDto responseDto = userService.registerUser(dto);
 
         ctx.status(201).json(responseDto);
     }
 
+    private  static void findUser(Context ctx, String uuid) throws SQLException {
+        UserResponseDto responseDto = userService.findUser(uuid);
+
+        ctx.status(200).json(responseDto);
+    }
+
+    public static void updateUser(Context ctx, String uuid, UserRequestDto dto) throws SQLException {
+        UserResponseDto responseDto = userService.updateUser(uuid, dto);
+
+        ctx.status(200).json(responseDto);
+    }
+
+    public static void deleteUser(Context ctx, String uuid) throws SQLException {
+        var response = userService.deleteUser(uuid);
+
+        ctx.status(200).json(response);
+    }
 }
