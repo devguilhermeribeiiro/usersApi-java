6aee17f3b0b630151961c11814fbc85609755b78
devguilhermeribeiro <devguilhermeribeiro000@gmail.com>
2025-05-14 01:06:54 -0300

    Adding other's rest methods to service

--- app/src/main/java/com/users/api/Service/UserService.java
+++ app/src/main/java/com/users/api/Service/UserService.java
@@ -2,12 +2,12 @@ package com.users.api.Service;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.UUID;
 
 import com.users.api.Dto.UserRequestDto;
 import com.users.api.Dto.UserResponseDto;
 import com.users.api.Entity.User;
 import com.users.api.Interfaces.RepositoryInterface;
-import com.users.api.Repository.UserRepository;
 
 public class UserService {
     private final RepositoryInterface userRepository;
@@ -16,25 +16,35 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public List<UserResponseDto> getActives() throws  SQLException {
+    public List<UserResponseDto> getUsers() throws  SQLException {
         List<UserResponseDto> allUsers;
-        List<UserResponseDto> usersActives;
 
-        allUsers = userRepository.findAll();
-        /* Implements filter to get only active users.
-        *
-        * for ( user : allUsers) {
-        *   if (user.active) { usersActives.add(user); }
-        * }
-        * */
-        return allUsers;
+        return  allUsers = userRepository.findAll();
     }
 
     public UserResponseDto registerUser(UserRequestDto userDto) throws SQLException {
         User user;
-        user = new User(userDto.name(), userDto.email());
+        user = new User(null, userDto.name(), userDto.email());
 
         return userRepository.save(user);
+    }
+
+    public UserResponseDto findUser(String id) throws SQLException {
+        UUID uuid = UUID.fromString(id);
+        return  userRepository.find(uuid);
+    }
+
+    public UserResponseDto updateUser(String id, UserRequestDto userRequestDto) throws SQLException {
+        UUID uuid = UUID.fromString(id);
+        User user = new User(uuid, userRequestDto.name(), userRequestDto.email());
+
+        return userRepository.edit(user);
+    }
+
+    public UserResponseDto deleteUser(String id) throws SQLException {
+        UUID uuid = UUID.fromString(id);
+
+        return  userRepository.destroy(uuid);
     }
 
 }
